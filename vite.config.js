41c49ba8cfb86eea9d006b65ea9ad4d5b610c2e0
Nitesh Giri <niteshgiri068@gmail.com>
2025-04-18 09:47:40 +0530

    set up base path for GitHub deplyment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
